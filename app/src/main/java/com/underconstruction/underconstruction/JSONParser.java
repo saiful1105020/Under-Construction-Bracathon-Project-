d5f2d9481f880dce61765ca225701ea18d2d5cb5
WasifurRahman <rahmanwasifur@gmail.com>
2016-04-11 19:36:28 +0600

    internal database can fetch,show,select and send the selected one to database without UI

--- app/src/main/java/com/underconstruction/underconstruction/JSONParser.java
+++ app/src/main/java/com/underconstruction/underconstruction/JSONParser.java
@@ -40,12 +40,13 @@ public class JSONParser {
 //        final String BASE_URL = "http://" + Utility.CurrentUser.getIp() +
 //                "/uc_brac_git/uc_server/index.php/home";
 
-     final String BASE_URL = "http://" + "172.20.62.23" +
+     /*final String BASE_URL = "http://" + "172.20.62.23" +
               "/uc_brac_git/uc_server/index.php/home";
+              */
 
-       /* final String BASE_URL = "http://" + "172.20.61.56" +                //Alternate Database
+        final String BASE_URL = "http://" + "172.20.62.18" +                //Alternate Database
                 "/hackThon/UC_Server/index.php/home";
-                */
+
 
         Log.d("base url", BASE_URL);
         URL url;
