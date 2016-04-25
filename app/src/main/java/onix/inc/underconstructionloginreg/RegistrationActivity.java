5deb6081736b34b0667e6a12edf9e2ee5a83b6f2
OnixHoque <onix.hoque@gmail.com>
2016-04-25 11:03:11 +0600

    Entire LoginReg Module

---
+++ app/src/main/java/onix/inc/underconstructionloginreg/RegistrationActivity.java
@@ -0,0 +1,208 @@
+package onix.inc.underconstructionloginreg;
+
+import android.app.Activity;
+import android.content.Intent;
+import java.util.ArrayList;
+import java.util.List;
+import android.util.Pair;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegistrationActivity extends Activity {
+
+    public String email, username, password, errorMessage, repeatPassword;
+    public TextView errorText;
+    public Button btnRegistration;
+    public LinearLayout layoutWait;
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    boolean signUpError;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        layoutWait = (LinearLayout) findViewById(R.id.layoutRegistrationWait);
+        btnRegistration = (Button) findViewById(R.id.btnRegistrationSignup);
+        signUpError=false;
+        errorText = (TextView) findViewById(R.id.lblRegistrationError);
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email = ((EditText) findViewById(R.id.txtRegistrationEmail)).getText().toString();
+                password = ((EditText) findViewById(R.id.txtRegistrationPassword)).getText().toString();
+                username = ((EditText) findViewById(R.id.txtRegistrationUsername)).getText().toString();
+                repeatPassword= ((EditText) findViewById(R.id.txtRegistrationConfirmPassword)).getText().toString();
+                if(showSignUpError()){
+                    errorText.setText("One or more required fields are missing!\n");
+                }else if(!validate(email)) {
+                    errorText.setText("Please enter a valid email address.\n");
+                }
+                else if(passwordMissMatch()){
+                    errorText.setText("Password Mismatch!\n");
+                }
+                else
+                {
+                    //Log.e("EMAIL", email + "\n" + password + "\n" + username + "\n"  + "\n");
+                    errorText.setText("");
+                    busy_session(true);
+                    SignUpTask signUpTask = new SignUpTask();
+                    signUpTask.execute();
+
+                }
+
+            }
+        });
+    }
+
+    class SignUpTask extends AsyncTask<String, Void, String> {
+
+        private JSONObject jsonSignUp, jsonLocations;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+        protected String doInBackground(String... args) {
+
+            JSONParser jParser = new JSONParser();
+            // Building Parameters
+            List<Pair> params = new ArrayList<Pair>();
+            params.add(new Pair("userName",username));
+            params.add(new Pair("email",email));
+            params.add(new Pair("password",password));
+            // getting JSON string from URL
+            jsonSignUp = jParser.makeHttpRequest("/register", "GET", params);
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         **/
+        protected void onPostExecute (String file_url){
+            if(jsonSignUp == null) {
+                //Utility.CurrentUser.showConnectionError(getApplicationContext());
+                errorText.setText("Please check your internet connection");
+                btnRegistration.setEnabled(true);
+                layoutWait.setVisibility(View.INVISIBLE);
+                return;
+            }
+            String loginStatus = new String ("");
+            try {
+                loginStatus = jsonSignUp.getString("status");
+                String uid = jsonSignUp.getString("userId");
+                Utility.CurrentUser.setUserId(uid);
+            }catch(JSONException e){
+                e.printStackTrace();
+            }
+
+            if(loginStatus.equals("0")){
+                errorText.setText("Sorry, this email already exists.");
+                busy_session(false);
+                return;
+            }
+            else{
+                finish();
+                Intent intent=new Intent(RegistrationActivity.this,VerificationActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+
+    /**
+     * Disable Registration button when the app is working
+     * @return
+     */
+    void busy_session(boolean flag)
+    {
+        if (flag)
+        {
+            btnRegistration.setEnabled(false);
+            layoutWait.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            ((EditText) findViewById(R.id.txtRegistrationEmail)).requestFocus();
+            ((EditText) findViewById(R.id.txtRegistrationEmail)).selectAll();
+            //((EditText) findViewById(R.id.txtRegistrationPassword)).setText("");
+            //((EditText) findViewById(R.id.txtRegistrationConfirmPassword)).setText("");
+            //((EditText) findViewById(R.id.txtRegistrationUsername)).setText("");
+
+            btnRegistration.setEnabled(true);
+            layoutWait.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    /**
+     * Check if password == repeat password
+     * @return
+     */
+    private boolean passwordMissMatch() {
+        //Log.d("signupcheck",password +" "+repeatPassword);
+        if(password.equals(repeatPassword))return false;
+        return true;
+
+    }
+
+    /**
+     * Check if email is valid
+     * @return
+     */
+    public static boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
+        return matcher.find();
+    }
+
+    /**
+     * Check if all fields are proper
+     * @return
+     */
+    private boolean showSignUpError(){
+
+
+        if(email.isEmpty() || password.isEmpty() || username.isEmpty() || repeatPassword.isEmpty()){
+
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_registration, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/onix/inc/underconstructionloginreg/Utility.java
@@ -0,0 +1,142 @@
+package onix.inc.underconstructionloginreg;
+
+/**
+ * Created by Anik on 3/14/2016.
+ */
+import android.content.Context;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by Shabab on 12/5/2015.
+ */
+
+public class Utility {
+
+    public static class CurrentUser{
+
+        private static String userId = "1";
+
+        private static int id=Integer.valueOf(userId), displayPage = 0;
+        private static String apiKey,username="@string/user_name";
+        public static boolean ipOK = false;
+        private static boolean valid=false;
+        private static String ip = "172.20.62.23";
+
+        public static void setUser(int i,String uName,String apikey){
+            id=i;
+            username=uName;
+            apiKey=apikey;
+            valid=true;
+        }
+
+        public static String getUsername() {
+            return username;
+        }
+
+        public static void setUsername(String username) {
+            CurrentUser.username = username;
+        }
+
+        public static String getUserId() {
+            return userId;
+        }
+
+        public static void setUserId(String userId) {
+            CurrentUser.userId = userId;
+        }
+
+        public static String getIp() {
+            return ip;
+        }
+
+        public static void setIp(String ip) {
+            CurrentUser.ip = ip;
+        }
+
+        public static void invalidate(){
+            valid=false;
+        }
+
+        public static  boolean isTheUserValid(){
+            return valid;
+        }
+
+        public static int getId(){return id;}
+        public static String getName(){return username;}
+
+        public static String getApiKey() {
+            return apiKey;
+        }
+
+        public static String makeString(){
+            return ""+id+" "+username+" "+apiKey;
+        }
+
+        public static int getDisplayPage() {
+            return displayPage;
+        }
+
+        public static void setDisplayPage(int i) {
+            displayPage = i;
+        }
+
+        public static void showConnectionError(Context context) {
+            Toast.makeText(context, "Please check your internet connection", Toast.LENGTH_LONG).show();
+        }
+
+        public static String parsePostTime (String dbString) {
+            int hr = Integer.parseInt("" + dbString.charAt(11) + dbString.charAt(12));
+            String min = "" +  dbString.charAt(14) + dbString.charAt(15);
+            String timeOfDay;
+            if(hr>12) {
+                hr = hr%12;
+                timeOfDay = "pm";
+            }
+            else if(hr == 12) timeOfDay = "pm";
+            else timeOfDay = "am";
+
+            if(hr == 0) hr = 12;
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(new Date());
+            String today = dateFormat.format(cal.getTime());
+            String date = dbString.substring(0, 10);
+
+            if (today.equals(date)) {
+                date = " Today";
+            }
+            else {
+                cal.add(Calendar.DAY_OF_MONTH, -1);
+                String yesterday = dateFormat.format((cal.getTime()));
+//                Log.d("yesterday", yesterday);
+                if (yesterday.equals(date)) {
+                    date = " Yesterday";
+                } else {
+                    String monthString = new DateFormatSymbols().getMonths()[Integer.parseInt(date.substring(5,7))-1];
+//                    Log.d("monthString", monthString);
+                    date = " " + monthString.substring(0,3) + " " + date.substring(8,10);
+                }
+            }
+
+            String timeOfUpdate = hr + ":" + min + timeOfDay + date;
+            return timeOfUpdate;
+        }
+
+    }
+
+    public static class HazardTags{
+        private static String[] hazardTags={"Occupied Footpath","Open Dustbin","Open Manhole","Cluttered Electric Wires","Water Logging","Risky Intersection","No Street Light","Crime Prone Area","Damaged Road","Wrongway Traffic"};
+
+        public static String[] getHazardTags(){
+            return hazardTags;
+        }
+    }
+
+}
