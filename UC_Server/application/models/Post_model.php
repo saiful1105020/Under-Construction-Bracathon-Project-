bdee5c43dd84697b9c68d0408472bebdd5d6e3ea
Shabab <sskhan11@live.com>
2016-05-08 20:24:25 +0600

    post suggestion incomplete

--- UC_Server/application/models/Post_model.php
+++ UC_Server/application/models/Post_model.php
@@ -15,7 +15,7 @@ class Post_model extends CI_Model
 
 	/**
 		Find the most recent posts near a given location.
-		Add this clause "where CURRENT_TIMESTAMP < time + INTERVAL ? DAY" inside the query
+		--(Done)-Add this clause "where CURRENT_TIMESTAMP < time + INTERVAL ? DAY" inside the query
 	*/
 	public function get_all_post($location_id)
 	{
@@ -25,6 +25,7 @@ class Post_model extends CI_Model
 	}
 	
 	/*
+	--Unnecessary now
 	public function get_all_post_backup($location_id)
 	{
 		$sql='select p.*,u.user_name,u.user_rating from post p, user u where p.actual_location_id = ? and p.status = 0 and p.user_id=u.user_id order by p.time desc';
@@ -34,6 +35,7 @@ class Post_model extends CI_Model
 	*/
 	
 	/**
+		--Unnecessary now
 		use user_id instead of user_name
 	*/
 	/*
@@ -48,6 +50,10 @@ class Post_model extends CI_Model
 	}
 	*/
 	
+	/**
+		Retrieve last 10 posts(pending/verified/rejected/solved) of a user
+		Used for generating rating graph and dashboard posts only. No other usage.
+	*/
 	public function get_last_10_user_post($user_id)
 	{
 		$sql = 'SELECT p.time,p.rating_change FROM post p WHERE p.user_id = ? ORDER BY p.time DESC LIMIT 10';
@@ -68,6 +74,15 @@ class Post_model extends CI_Model
 	}
 	
 	
+	/**
+		Used for login process.
+		Input: email-id and password
+		Output: userId, userName, isVerified
+		
+		userId: id of the user if found in database, if not found, return 0
+		userName: If not found, return ""
+		isVerified: 0 when the account is not verified(need to go through verification process), 1 represents a verified account.
+	*/
 	public function get_loginInfo($email,$password)
 	{
 		$sql="SELECT user_id as userId,user_name as userName,is_verified as isVerified FROM user WHERE email = ? and password = ?";
@@ -90,13 +105,20 @@ class Post_model extends CI_Model
 		return $data;
 	}
 	
+	/**
+		Mark a user as verified.
+	*/
 	public function verify($user_id)
 	{
 		$sql = "UPDATE user SET is_verified = 1 WHERE user_id = ?";
 		$query = $this->db->query($sql,array($user_id));
 	}
 	
-	public function is_duplicate($email)
+	/**
+		Return 0 when, the email is already taken/used
+		Return 1 if the email is not used
+	*/
+	public function is_email_available($email)
 	{
 		$sql="SELECT user_id FROM user WHERE email = ? ";
 		$query = $this->db->query($sql,array($email))->row_array();
@@ -110,26 +132,26 @@ class Post_model extends CI_Model
 		}
 	}
 	
+	/**
+		Tried to detect similar posts based on location(lat,long), time of post and category.
+		There is a huge room for improvement here.
+	*/
 	public function get_suggestions($lat,$lon,$time,$cat)
 	{
-		//$sql = 'SELECT u.user_name as userName , p.`category` as cat, p.`image` as img FROM `user` u,post p WHERE u.user_id = p.user_id';
-		
-		$sql = 'SELECT u.user_name as userName, p.`category` as cat, p.`time` as time, p.`text` as txt, 
-				l.street_number as streetNo,l.route as route,l.neighbourhood as nighborhood,l.sublocality as sublocality,l.locality as locality,
-				p.`rating_change` as voteCount
+		$sql = 'SELECT u.user_id as user_id,u.user_rating as user_rating,u.user_name as user_name, p.*,
+				l.street_number as streetNo,l.route as route,l.neighbourhood as neighbourhood,l.sublocality as sublocality,l.locality as locality
 				FROM `user` u,`post` p, `location` l 
 				WHERE u.user_id = p.user_id AND l.location_id = p.actual_location_id AND p.category = ? 
 				AND abs(l.`lat` - ?) <= 0.0004 AND abs(l.`lon` - ?) <=0.0004
 				AND datediff(p.time,?)<=7 AND p.status =0';
 				
 		$result = $this->db->query($sql,array($cat,$lat,$lon,$time))->result_array();
-		//$result = $this->db->query($sql)->result_array();
 		
 		return $result;
 	}
 	
 	/**
-		Returns user_id
+		Returns user_id when the registration is complete
 	*/
 	public function register($data)
 	{
@@ -145,6 +167,9 @@ class Post_model extends CI_Model
 		return $query['user_id'];
 	}
 	
+	/**
+		Returns the verification code for a user
+	*/
 	public function get_verification_code($user_id)
 	{
 		$sql = "SELECT `ver_code` FROM `user` WHERE `user_id` = ? ";
@@ -154,6 +179,7 @@ class Post_model extends CI_Model
 	
 	
 	/*
+	unnecessary now
 	public function get_user_posts($user_name)
 	{
 		$sql = 'select p.* from post p, user u where u.user_name = ? and p.user_id=u.user_id order by p.time desc';
@@ -162,6 +188,9 @@ class Post_model extends CI_Model
 	}
 	*/
 	
+	/**
+		Returns posts of a user (limitted to 20 posts only. You may change it if you need).
+	*/
 	public function get_user_posts($user_id)
 	{
 		$sql = 'select p.* from post p where p.user_id=? order by p.time desc limit 20';
@@ -169,6 +198,9 @@ class Post_model extends CI_Model
 		return $query;
 	}
 	
+	/**
+		Count total upvotes and downvotes for a post.
+	*/
 	public function get_vote_count($post_id)
 	{
 		$sql='SELECT COUNT(*) as upvotes FROM vote WHERE post_id=? and vote_type=1';
@@ -184,13 +216,21 @@ class Post_model extends CI_Model
 		return $data;
 	}
 	
+	/**
+		Return user_id against a user_name
+		--no usage of this function. We should not use it as user_name may not be unique
+	
 	public function get_user_id($user_name)
 	{
 		$sql="SELECT user_id FROM user WHERE user_name = ? ";
 		$query=$this->db->query($sql,$user_name)->row_array();
 		return $query['user_id'];
 	}
+	*/
 	
+	/**
+		Returns a list of user_id's who either upvoted or downvoted a post
+	*/
 	public function get_voters($post_id)
 	{
 		$result=array();
@@ -206,6 +246,9 @@ class Post_model extends CI_Model
 	}
 	
 	/**
+		If the user has already voted for this post, just change (update) the vote type
+		Otherwise, insert the new vote
+	
 		VOTE_TYPE:
 			1   -> upvote
 			-1	-> downvote
@@ -228,7 +271,7 @@ class Post_model extends CI_Model
 	}
 	
 	/**
-		WASIF
+		Insert a new post
 	*/
 	public function insert_post($post)
 	{
@@ -237,7 +280,7 @@ class Post_model extends CI_Model
 	}
 	
 	/**
-		Use user_id instead of user_name
+		Returns current total rating of a user
 	*/
 	public function get_current_rating($user_id)
 	{
@@ -247,7 +290,9 @@ class Post_model extends CI_Model
 	}
 	
 	/**
-	Database Error??
+		Try to match an existing location in database with user's location.
+		If found, return location_id
+		Otherwise, return -1 to indicate no found
 	*/
 	public function get_location_id($lat,$lon)
 	{
@@ -262,7 +307,13 @@ class Post_model extends CI_Model
 		else return -1;
 	}
 	
-	//insert location and get location id
+	/**
+		Insert(?) location and return location_id
+		
+		First, try to match an existing location in database
+		If found, no need to insert again -- just return the location_id
+		Otherwise, Just return the location_id matched
+	*/
 	public function insert_location($data)
 	{
 		$sql='SELECT location_id FROM location WHERE abs(`lat` - ?) <= 0.002 AND abs(`lon` - ?) <=0.002';
@@ -287,6 +338,9 @@ class Post_model extends CI_Model
 		}
 	}
 	
+	/**
+		Returns location details for a location_id
+	*/
 	public function get_location($location_id)
 	{
 		$sql='SELECT * FROM location WHERE location_id = ?';
@@ -295,7 +349,7 @@ class Post_model extends CI_Model
 	}
 	
 	/**
-		UNTESTED
+		Returns the list of category
 	*/
 	public function get_category_list()
 	{
@@ -304,6 +358,9 @@ class Post_model extends CI_Model
 		return $query;
 	}
 	
+	/**
+		Returns the list of category name suggested by the users
+	*/
 	public function get_suggested_category_list()
 	{
 		$sql='SELECT * FROM suggestedCategory';
@@ -311,18 +368,28 @@ class Post_model extends CI_Model
 		return $query;
 	}
 	
+	/**
+		Increment count field of a suggested category
+	*/
 	public function inc_suggestion_count($duplicateId)
 	{
 		$sql = 'UPDATE `suggestedcategory` SET `count`=`count` WHERE `id`= ?';
 		$query=$this->db->query($sql,array($duplicateId));
 	}
 	
+	/**
+		Insert a suggested category
+	*/
 	public function insert_cat_sugegstion($newCat)
 	{
 		$sql = 'INSERT INTO `suggestedcategory`(`name`, `count`) VALUES (?,1)';
 		$query=$this->db->query($sql,array($newCat));
 	}
 	
+	/**
+		Returns 1 if the category name ($cat) matches with any of the categories in database
+		Otherwise, Return 0
+	*/
 	public function existCategory($cat)
 	{
 		$sql = 'SELECT COUNT(*) as cnt FROM category WHERE `categoryId` = ?';
